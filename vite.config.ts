cbe5ab6e5ca97f4f92c55a96c4d4a2671b72ac93
zjm925 <zhangjinming925@163.com>
2025-12-18 15:58:28 +0800

    change to new

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import { componentTagger } from 'lovable-tagger'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
-  plugins: [
-    react(),
-    mode === 'development' && componentTagger(),
-  ].filter(Boolean),
+export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
     }
   },
   server: {
-    host: '::',
-    port: 8080,
+    port: 5800,
     proxy: {
       '/base': {
         target: 'http://localhost:8002',
@@ -30,4 +25,4 @@ export default defineConfig(({ mode }) => ({
       }
     }
   }
-}))
+})
